689af69297e41c0a581e94706402de65379c9861
yjw634448759 <634448759@qq.com>
2016-12-21 14:39:24 +0800

    开店功能

---
+++ src/main/java/com/cloudage/membercenter/entity/Shop.java
@@ -0,0 +1,45 @@
+package com.cloudage.membercenter.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import com.cloudage.membercenter.util.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+@Entity
+public class Shop extends BaseEntity{
+	String shopName;	// 店铺名称
+	String description;	// 简介
+	String shopImage;	// 店铺图片
+	User owner;			// 店家
+	@Column(nullable=false)
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String getDecription() {
+		return description;
+	}
+	public void setDecription(String description) {
+		this.description = description;
+	}
+	@OneToOne(optional=false)
+	@JsonIgnore
+	public User getOwner() {
+		return owner;
+	}
+	public void setOwner(User owner) {
+		this.owner = owner;
+	}
+	@Column(nullable=true)
+	public String getShopImage() {
+		return shopImage;
+	}
+	public void setShopImage(String shopImage) {
+		this.shopImage = shopImage;
+	}
+
+
+}
---
+++ src/main/java/com/cloudage/membercenter/service/DefaultShopService.java
@@ -0,0 +1,21 @@
+package com.cloudage.membercenter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cloudage.membercenter.entity.Shop;
+import com.cloudage.membercenter.repository.IShopRepository;
+@Component
+@Service
+@Transactional
+public class DefaultShopService implements IShopService {
+	@Autowired
+	IShopRepository shopRepo;
+	@Override
+	public Shop save(Shop shop) {
+		return shopRepo.save(shop);
+	}
+
+}
