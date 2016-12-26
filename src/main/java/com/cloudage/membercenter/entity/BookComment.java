0a33ec260c99f08e4b6a9d2797d759cb1fdca5ca
GuanVin <905150371@qq.com>
2016-12-26 10:42:56 +0800

    ..

--- src/main/java/com/cloudage/membercenter/entity/BookComment.java
+++ src/main/java/com/cloudage/membercenter/entity/BookComment.java
@@ -7,46 +7,50 @@ import javax.persistence.Transient;
 import com.cloudage.membercenter.util.DateRecord;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 public class BookComment extends DateRecord {
 
-	String text;//评论
-	User author;//评论者
-	Goods goods;//商品
+        String text;// 评论
+        User author;// 评论者
+        Goods goods;// 商品
 
+        public String getText() {
+                return text;
+        }
 
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
+        public void setText(String text) {
+                this.text = text;
+        }
 
-	@ManyToOne(optional=false)
-	@JsonIgnore
-	public User getAuthor() {
-		return author;
-	}
-	public void setAuthor(User author) {
-		this.author = author;
-	}
-	@Transient
-	public String getAuthorName(){
-		return author.name;
-	}
-	@ManyToOne(optional=false)
-	@JsonIgnore
-	public Goods getGoods() {
-		return goods;
-	}
-	public void setGoods(Goods goods) {
-		this.goods = goods;
-	}
-	@Transient
-	public int getGoodsId(){
-		return goods.getId();
-	}
+        @ManyToOne(optional = false)
+        @JsonIgnore
+        public User getAuthor() {
+                return author;
+        }
 
+        public void setAuthor(User author) {
+                this.author = author;
+        }
 
+        @Transient
+        public String getAuthorName() {
+                return author.name;
+        }
+
+        @ManyToOne(optional = false)
+        @JsonIgnore
+        public Goods getGoods() {
+                return goods;
+        }
+
+        public void setGoods(Goods goods) {
+                this.goods = goods;
+        }
+
+        @Transient
+        public int getGoodsId() {
+                return goods.getId();
+        }
 
 }
