6a28e78d5c0444f5e8c26c497cfacda09f98a56a
yjw634448759 <634448759@qq.com>
2017-01-03 21:08:33 +0800

    添加商品评论

--- src/main/java/com/cloudage/membercenter/service/IBookCommentService.java
+++ src/main/java/com/cloudage/membercenter/service/IBookCommentService.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import com.cloudage.membercenter.entity.BookComment;
 
 public interface IBookCommentService {
-
+	void save(BookComment comment);
 	Page<BookComment> findAllCommentsByBookId(int goods_id, int page);
 
 	//Page<BookComment> findAllCommentsByBookName(String goods_name, int page);
